994c471e86617485bad60d8dff82f8b5d51a4e34
JITENDRA SINGH BISHT <152790288+vickybisht001@users.noreply.github.com>
2025-07-23 10:52:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,10 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-window.addEventListener("scroll", function () {
-  let navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("sticky",this.window.scrollY>0);
-  navbar.classList.toggle("bg-color",this.window.scrollY>0);
-});
-
 reportWebVitals();
